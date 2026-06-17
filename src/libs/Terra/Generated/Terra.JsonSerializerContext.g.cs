1fe0be388af86e41832296eb7ae5e33aec045c1b
Konstantin S. <havendv@gmail.com>
2026-06-17 17:56:18 +0400

    fix: use generated empty json context

--- src/libs/Terra/Generated/Terra.JsonSerializerContext.g.cs
+++ src/libs/Terra/Generated/Terra.JsonSerializerContext.g.cs
@@ -17,5 +17,47 @@ namespace Terra
         })]
     public sealed partial class SourceGenerationContext : global::System.Text.Json.Serialization.JsonSerializerContext
     {
+        private static readonly global::System.Text.Json.JsonSerializerOptions DefaultOptions = CreateDefaultOptions();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static SourceGenerationContext Default { get; } = new(DefaultOptions);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SourceGenerationContext()
+            : this(DefaultOptions)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SourceGenerationContext(global::System.Text.Json.JsonSerializerOptions? options)
+            : base(options)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override global::System.Text.Json.JsonSerializerOptions? GeneratedSerializerOptions => DefaultOptions;
+
+        /// <inheritdoc />
+        public override global::System.Text.Json.Serialization.Metadata.JsonTypeInfo? GetTypeInfo(global::System.Type type)
+        {
+            return null;
+        }
+
+        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+        {
+            var options = new global::System.Text.Json.JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+            };
+            options.Converters.Add(new global::Terra.JsonConverters.UnixTimestampJsonConverter());
+
+            return options;
+        }
     }
 }
